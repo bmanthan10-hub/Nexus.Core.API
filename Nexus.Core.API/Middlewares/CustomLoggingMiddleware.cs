c25f7da360418ab7972d09febaa582f24bd79de7
bmanthan <bmanthan10@gmail.com>
2026-04-24 15:41:00 +0530

    Finalizing Assignment 2: Middleware and Logging

---
+++ Nexus.Core.API/Middlewares/CustomLoggingMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Nexus.Core.API.Middlewares
+{
+    public class CustomLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Requirement: Log path and current time for every request
+            string path = context.Request.Path;
+            string time = DateTime.Now.ToString("hh:mm tt");
+
+            Console.WriteLine($"-> Path: {path} | Time: {time}");
+
+            await _next(context);
+        }
+    }
+}
